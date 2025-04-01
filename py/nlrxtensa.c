cc86663848053f9471126c80afd5d35267797f6c
Paul Schulze <pschulze@helindustries.com>
2025-04-01 02:31:26 +0200

    Fix unsafe jump linker error

--- py/nlrxtensa.c
+++ py/nlrxtensa.c
@@ -49,7 +49,7 @@ unsigned int nlr_push(nlr_buf_t *nlr) {
         "s32i.n  a13, a2, 36        \n"
         "s32i.n  a14, a2, 40        \n"
         "s32i.n  a15, a2, 44        \n"
-        "j      nlr_push_tail       \n" // do the rest in C
+        "call0   nlr_push_tail       \n" // do the rest in C
         );
 
     return 0; // needed to silence compiler warning
